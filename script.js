64b26062f8f91c5145254efb6b5bd56e7e69f445
KevinW5 <kevinw267@nycstudents.net>
2024-06-09 16:54:57 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+// script.js
+
+const slides = document.querySelectorAll(".slide");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+let currentSlide = 0;
+
+function showSlide(n) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[n].classList.add("active");
+}
+
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+  showSlide(currentSlide);
+}
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
+
+// Initial slide
+showSlide(currentSlide);
+
+// Change background color of the web page 
+document.body.style.backgroundColor = "#445"; 
